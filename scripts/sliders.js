65034f06fbbfb809e6fae328b725a0f3fd1cf4a3
JuliusMoeller <64658997+JuliusMoeller@users.noreply.github.com>
2020-05-28 22:41:17 +0200

    Finished adaptable Sliders

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -4,7 +4,7 @@ const heroprev = document.querySelector(".heroprev");
 const heronext = document.querySelector(".heronext");
 const heroindicator = document.querySelector(".heroindicator");
 const herotime = 5000;
-let heroindex = 0;
+var heroindex = 0;
 
 
 /* Game Display Slider Constants */
@@ -13,49 +13,63 @@ const gdprev = document.querySelector(".gdprev");
 const gdnext = document.querySelector(".gdnext");
 const gdindicator = document.querySelector(".gdindicator");
 const gdtime = 8000;
-let gdindex = 0;
+var gdindex = 0;
 
 
 /* Prev/Next Button Event Listeners********************************************/
+/* Hero */
+heroprev.addEventListener("click", function(){
+  prevSlide(heroslides, "heroindex");
+  updateCircleIndicator("heroindex", heroindicator);
+  resetTimer("herotimer", herotime, heroslides, "heroindex", heroindicator);
+});
+
+heronext.addEventListener("click", function(){
+  nextSlide(heroslides, "heroindex");
+  updateCircleIndicator("heroindex", heroindicator);
+  resetTimer("herotimer", herotime, heroslides, "heroindex", heroindicator);
+});
+
+/* Game Display */
 gdprev.addEventListener("click", function(){
-  prevSlide(gdslides, gdindex);
-  updateCircleIndicator(gdindex, gdindicator);
-  resetTimer(gdtimer, gdtime, gdslides, gdindex, gdindicator);
+  prevSlide(gdslides, "gdindex");
+  updateCircleIndicator("gdindex", gdindicator);
+  resetTimer("gdtimer", gdtime, gdslides, "gdindex", gdindicator);
 });
 
 gdnext.addEventListener("click", function(){
-  nextSlide(gdslides, gdindex);
-  updateCircleIndicator(gdindex, gdindicator);
-  resetTimer(gdtimer, gdtime, gdslides, gdindex, gdindicator);
+  nextSlide(gdslides, "gdindex");
+  updateCircleIndicator("gdindex", gdindicator);
+  resetTimer("gdtimer", gdtime, gdslides, "gdindex", gdindicator);
 });
 
 /* Circle Indicators **********************************************************/
 function circleIndicator(){
-  for(let i = 0; i < gdslides.length; i++){
-    const div = document.createElement("div");
-    div.addEventListener("click", function(){
-      indicateSlide(gdtimer, gdtime, gdslides, gdindex, gdindicator, i);
-    });
-    if(i == 0){
-      div.className="active";
-    }
-    gdindicator.appendChild(div);
-  }
   for(let i = 0; i < heroslides.length; i++){
     const div = document.createElement("div");
     div.addEventListener("click", function(){
-      indicateSlide(herotimer, herotime, heroslides, heroindex, heroindicator, i);
+      indicateSlide("herotimer", herotime, heroslides, "heroindex", heroindicator, i);
     });
     if(i == 0){
       div.className="active";
     }
     heroindicator.appendChild(div);
   }
+  for(let i = 0; i < gdslides.length; i++){
+    const div = document.createElement("div");
+    div.addEventListener("click", function(){
+      indicateSlide("gdtimer", gdtime, gdslides, "gdindex", gdindicator, i);
+    });
+    if(i == 0){
+      div.className="active";
+    }
+    gdindicator.appendChild(div);
+  }
 }
 circleIndicator();
 
 function indicateSlide(timer, time, slides, index, indicator, id){
-  index = id;
+  window[index] = id
   changeSlide(slides, index);
   updateCircleIndicator(index, indicator);
   resetTimer(timer, time, slides, index, indicator);
@@ -65,23 +79,23 @@ function updateCircleIndicator(index, indicator){
   for(let i = 0; i < indicator.children.length; i++){
     indicator.children[i].classList.remove("active");
   }
-  indicator.children[index].classList.add("active");
+  indicator.children[window[index]].classList.add("active");
 }
 
 function prevSlide(slides, index){
-  if(index == 0){
-    index = slides.length - 1;
+  if(window[index] == 0){
+    window[index] = slides.length - 1;
   }else{
-    index--;
+    window[index]--;
   }
   changeSlide(slides, index);
 }
 
 function nextSlide(slides, index){
-  if(index == slides.length -1){
-    index = 0;
+  if(window[index] == slides.length -1){
+    window[index] = 0;
   }else{
-    index++;
+    window[index]++;
   }
   changeSlide(slides, index);
 }
@@ -90,12 +104,12 @@ function changeSlide(slides, index){
   for(let i = 0; i < slides.length; i++){
     slides[i].classList.remove("active");
   }
-  slides[index].classList.add("active");
+  slides[window[index]].classList.add("active");
 }
 
 function resetTimer(timer, time, slides, index, indicator){
-  clearInterval(timer);
-  timer = setInterval(autoplay, time, slides, index, indicator);
+  clearInterval(window[timer]);
+  window[timer] = setInterval(autoplay, time, slides, index, indicator);
 }
 
 function autoplay(slides, index, indicator){
@@ -104,7 +118,7 @@ function autoplay(slides, index, indicator){
 }
 
 //Hero Timer
-let herotimer = setInterval(autoplay, herotime, heroslides, heroindex, heroindicator);
+var herotimer = setInterval(autoplay, herotime, heroslides, "heroindex", heroindicator);
 
 //Game Display Timer
-let gdtimer = setInterval(autoplay, gdtime, gdslides, gdindex, gdindicator);
+var gdtimer = setInterval(autoplay, gdtime, gdslides, "gdindex", gdindicator);
